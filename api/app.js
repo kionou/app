7dd82d190e76acdc4780d8eb3137995093e9b90d
kionou <kionoumamadou.00@gmail.com>
2023-04-08 05:43:58 +0000

    ok

--- api/app.js
+++ api/app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.use(cors())
 
 app.use('/', indexRouter);
@@ -39,5 +40,15 @@ app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error');
 });
+if (process.env.NODE_ENV === 'production') {
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+
+  app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
 
 module.exports = app;
